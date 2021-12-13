3a4a6ace5990f295f2dfffe84471f9db418e0abb
rloterh <rloterh@gmail.com>
2021-12-13 05:30:04 +0000

    Update api url

--- src/utilities/getData.js
+++ src/utilities/getData.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getInitialFeed = async () => {
-  const response = await axios.get('http://localhost:300/v1/feeds');
+  const response = await axios.get('http://localhost:3000/api/v1/feeds');
   return response.data;
 };
 
