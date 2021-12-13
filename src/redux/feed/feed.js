fb8b58531b78a1942e2043ac8eb56f0309ac09c1
rloterh <rloterh@gmail.com>
2021-12-13 03:15:41 +0000

    Add feed actions and reducer

---
+++ src/redux/feed/feed.js
@@ -0,0 +1,31 @@
+import getInitialFeed from '../../utilities/getData';
+
+//Initial State
+const initialState = {
+  feed: '',
+};
+
+//Action
+export const GET_FEED_REQUEST = 'feed/GET_FEED_REQUEST';
+
+//Action Creators
+export const getFeed = async (dispatch) => {
+  const feed = await getInitialFeed();
+  return dispatch({
+    type: GET_FEED_REQUEST,
+    payload: feed,
+  });
+};
+
+//Reducer
+const feedReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_FEED_REQUEST:
+      return { ...state, feed: action.payload };
+
+      default:
+        return state;
+  }
+}
+
+export default feedReducer;
