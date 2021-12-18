eaf94042302c2dea08f267d29d7f17b43f211f82
rloterh <rloterh@gmail.com>
2021-12-18 15:27:11 +0000

    Add actions + promise > sprints

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,3 +25,38 @@ export const signUserUp = (userInfo) => (dispatch) => {
     })
     .catch((error) => error);
 };
+
+export const getSprint = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: 'GET_SPRINT_REQUEST',
+    });
+    const response = await axios.get('http://localhost:3000/api/v1/sprints');
+
+    dispatch({
+      type: 'GET_SPRINT_SUCCESS',
+      payload: response.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: 'LISTS_FAIL',
+    });
+  }
+};
+
+export const getSingleSprint = (sprint) => async (dispatch) => {
+  try {
+    dispatch({
+      type: 'GET_SINGLE_SPRINT_REQUEST',
+    });
+
+    dispatch({
+      type: 'GET_SINGLE_SPRINT_REQUEST',
+      payload: [sprint],
+    });
+  } catch (e) {
+    dispatch({
+      type: 'SINGLE_LIST_FAIL',
+    });
+  }
+};
